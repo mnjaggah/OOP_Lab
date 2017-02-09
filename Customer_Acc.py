a0add32bd471d44285b218c084b8ffad3cb42d3b
Maria <maria.njaggah@gmail.com>
2017-02-09 04:19:35 +0300

    modified subclass for efficient inheritance

--- Customer_Acc.py
+++ Customer_Acc.py
@@ -1,6 +1,6 @@
 #create a main class that will interact with a number of objects
 class Customer_Acc(object):
-    def __init__ (self, name, balance = 0.0):
+    def __init__ (self, name, balance):
         self.balance = balance
         self.name = name
 
@@ -12,23 +12,26 @@ class Customer_Acc(object):
 
     def withdraw(self, amount):
         self.balance -= amount
-        return self.balance
+        if self.balance < 0:
+            return "Warning!You are in debt",self.balance
+        else:
+            return self.balance
     
     def check_balance(self):
         return self.balance
     
     #create a class that specifies the minimum withdrawal that inherites
-    #from the mainn class Customer_Acc
+    #from the main class Customer_Acc
 
 
 class Minimum_Customer_Acc_Bal(Customer_Acc):
-    def __init__(self, min_balance = 20):
-        Customer.__init__(self, name, balance = 0.0)
-        self.min_balance = min_balance
+    def __init__(self, name, balance):
+        Customer_Acc.__init__(self, name, balance)
+        self.min_balance = balance
 
     def withdraw(self, amount):
         if self.balance - amount < self.min_balance:
-            return "Your balance is insufficient."
+            return "You cannot withdraw!Your balance is insufficient."
         else:
             Customer_Acc.withdraw(amount)
 
